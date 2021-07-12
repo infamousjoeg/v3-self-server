c409f21d4e22f9821ae3f58ecece17608ec3837b
Joe Garcia <joe@joe-garcia.com>
2021-07-12 17:21:11 -0400

    Add shellcheck fixes, mysql -e queries

--- install/cybrselfserve-install.sh
+++ install/cybrselfserve-install.sh
@@ -36,8 +36,8 @@ install-java() {
   JAVA_DIR=$(ls -ld /usr/lib/jvm/java*jdk* | grep -v ^l | awk '{print $9}')
   export JAVA_HOME=$JAVA_DIR 
   export JRE_HOME=$JAVA_DIR 
-  echo "export JAVA_HOME="$JAVA_HOME"" >> ~/.bashrc
-  echo "export JRE_HOME="$JRE_HOME"" >> ~/.bashrc
+  echo "export JAVA_HOME=$JAVA_HOME" >> ~/.bashrc
+  echo "export JRE_HOME=$JRE_HOME" >> ~/.bashrc
 }
 
 #############################
@@ -48,8 +48,8 @@ install-tomcat() {
   tar xzf apache-tomcat-9.0.$TOMCAT_MINOR_VERSION.tar.gz
   rm apache-tomcat-9.0.$TOMCAT_MINOR_VERSION.tar.gz
   sudo mv apache-tomcat-9.0.$TOMCAT_MINOR_VERSION /usr/local/tomcat9
-  echo "export CATALINA_HOME="/usr/local/tomcat9"" >> ~/.bashrc
-  source ~/.bashrc
+  echo "export CATALINA_HOME=/usr/local/tomcat9" >> ~/.bashrc
+  source "${HOME}/.bashrc"
 
   # copy tomcat-users.xml file w/ admin rights for UI (see end of file)
   cp ./tomcat-users.xml /usr/local/tomcat9/conf/tomcat-users.xml
@@ -58,7 +58,7 @@ install-tomcat() {
   cp ../dependencies/mysql-connector-java-8.0.25.jar /usr/local/tomcat9/lib
 
   # start tomcat
-  cd /usr/local/tomcat9/bin
+  cd /usr/local/tomcat9/bin || exit
   chmod +x ./*.sh
   ./startup.sh
 }
@@ -81,27 +81,10 @@ install-mysql() {
   echo
   echo "waiting 10 seconds for db to initialize..."
   sleep 10
-  echo
-  echo
-  echo
-  echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-  echo "                         READ      THIS"
-  echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-  echo
-  echo "At the mysql prompt cut/paste, the following commands:"
-  echo
-  echo "  update user set authentication_string=PASSWORD('Cyberark1') where user='root';"
-  echo "  update user set plugin=\"mysql_native_password\" where User='root';"
-  echo "  flush privileges;"
-  echo
-  echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-  echo "           LOOK     HERE  ^^^^^"
-  echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-  echo
-  echo
-  sudo mysql --user=root mysql 
+  sudo mysql --user=root \
+    -e "UPDATE user SET authentication_string='Cyberark1' WHERE user='root'; UPDATE user SET plugin='mysql_native_password' WHERE User='root'; FLUSH PRIVILEGES;"
   for i in $(ps -aux | grep mysql | grep -v grep | awk '{print $2}'); do
-    sudo kill -9 $i
+    sudo kill -9 "$i"
   done
   sudo service mysql start
 }
